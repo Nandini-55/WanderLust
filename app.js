5c7e79572e28b3cf81234be7087777541d091eb0
Nandini-55 <njkstars1212@gmail.com>
2026-03-29 21:24:05 +0530

    redirect root route to listings page

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.get("/", (req, res) => {
-  res.send("Hi , i am root");
+  res.redirect("/listings");
 });
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
